e0eb9fe843afa85c1b99123e5c6aa695de50e124
Anders Olsson <anders@Anderss-MacBook-Pro.local>
2018-12-23 02:14:11 +0100

    Fixed

--- Day13_2.py
+++ Day13_2.py
@@ -9,7 +9,7 @@ def print_tracks(matrix, trains):
             has_train = False
             i = 0
             while i < len(trains) and has_train == False:
-                if trains[i][3] == x and trains[i][4] == y:
+                if trains[i][3] == x and trains[i][4] == y and trains[i][5]:
                     tracks += trains[i][1]
                     has_train = True
                 i += 1
@@ -17,29 +17,33 @@ def print_tracks(matrix, trains):
             if has_train == False:
                 tracks += matrix[x][y]
     print(tracks)
+
+def get_trains_in_order(trains):
+    trains = list(filter(lambda t: t[5], trains))
+    trains.sort(key = lambda l: (l[4], l[3]))
+    return trains
         
 print("Advent of Code; day 13 task 2")
 
 debug = False
 debug_output = False
 
-if debug == True:
+if debug:
     print("DEBUG")
     file_name = "Day13Data_test2.txt" #6,4
 else:
     print("LIVE")
-    file_name = "Day13Data.txt"
+    file_name = "Day13Data.txt" #69,67
 
 data_file = open(file_name, "r")
 data = data_file.readlines()
 data_file.close()
 
-# crash_coord = (7, 3)
-
 width = len(data[0])
 height = len(data)
 
-print(width, height)
+if debug_output:
+    print(width, height)
 
 matrix = [[0 for x in range(height)] for y in range(width)]
 trains = []
@@ -49,49 +53,40 @@ for x in range(0, width):
     for y in range(0, height):
         char = data[y][x]
         if char == ">" or char == "<":
-            trains.append((train_count, char, "l", x, y))
+            trains.append((train_count, char, "l", x, y, True))
             train_count += 1
             char = "-"
         elif char == "v" or char == "^":
-            trains.append((train_count, char, "l", x, y))
+            trains.append((train_count, char, "l", x, y, True))
             train_count += 1
             char = "|"
-        # else:
-        #     if char == " ":
-        #         raise Exception("weird")
+
         matrix[x][y] = char
 
-print(trains)
+if debug_output:
+    print(trains)
 
-tracks = ""
-for y in range(0, height):
-    for x in range(0, width):
-        tracks += matrix[x][y]
-print(tracks)
+if debug_output:
+    tracks = ""
+    for y in range(0, height):
+        for x in range(0, width):
+            tracks += matrix[x][y]
+    print(tracks)
 
-print_tracks(matrix, trains)
-
-def get_first_train(trains):
-    smallest_y = None
-    smallest_x = None
-    first_train = None
-    for train in trains:
-        if smallest_y == None or train[4] < smallest_y:
-            if smallest_x == None or train[3] < smallest_x:
-                smallest_y = train[4]
-                smallest_x = train[3]
-                first_train = train
-    return first_train
+if debug_output:
+    print_tracks(matrix, trains)
 
 tick_count = 0
 final_coord = None
-while len(trains) > 1:
+
+while len(list(filter(lambda t: t[5], trains))) > 1:
     tick_count += 1
 
-    if tick_count % 1000 == 0:
+    if debug_output and tick_count % 1000 == 0:
         print(str(tick_count) + " ticks")
 
-    for i in range(0, len(trains)): #find top left train
+    trains = get_trains_in_order(trains)
+    for i in range(0, len(trains)):
         direction = trains[i][1]
         next_turn = trains[i][2]
         x = trains[i][3]
@@ -158,40 +153,32 @@ while len(trains) > 1:
                     new_direction = "v"
                 new_next_turn = "l"
 
-        trains[i] = (trains[i][0], new_direction, new_next_turn, next_x, next_y)
-    
-    new_trains = []
-    for train in trains:
-        train_x = train[3]
-        train_y = train[4]
         does_collide = False
-        for other_train in trains:
-            if train[0] != other_train[0]:
+        for j in range(0, len(trains)):
+            other_train = trains[j]
+            if trains[i][0] != other_train[0]:
                 other_train_x = other_train[3]
                 other_train_y = other_train[4]
 
-                if train_x == other_train_x and train_y == other_train_y:
-                    does_collide = True
-                    print("Trains " + str(train[0]) + " and " + str(other_train[0]) + " collided (" + str(train[3]) + "," + str(train[4]) + "). " + str(len(trains) - 2) + " trains left.")
-                    #print("Trains " + str(train[0]) + " and " + str(other_train[0]) + " collided.")
+                if next_x == other_train_x and next_y == other_train_y:
+                    if trains[i][5] and other_train[5]:
+                        does_collide = True
+                        trains[i] = (trains[i][0], new_direction, new_next_turn, next_x, next_y, False)
+                        trains[j] = (trains[j][0], trains[j][1], trains[j][2], trains[j][3], trains[j][4], False)
+                        print("Collision at " + str(next_x) + "," + str(next_y) + " after " + str(tick_count) + " ticks.")
+        
         if does_collide == False:
-            new_trains.append(train)
-
-    trains = new_trains
+            trains[i] = (trains[i][0], new_direction, new_next_turn, next_x, next_y, trains[i][5])
     
-    if debug_output == True:
+    if debug_output:
         print_tracks(matrix, trains)
 
+trains = get_trains_in_order(trains)
 final_coord = (trains[0][3], trains[0][4])
 
 if final_coord != None:
-    print_tracks(matrix, trains)
-    print("Results: " + str(final_coord[0]) + "," + str(final_coord[1]))
-
-#when trains crash, remove both
-#when trains crash, continue exection
-#when only one train left, get coordinates for the remaing train
-#not 138,89
-
-#rule of which train moves first!! It means, a cart that has moved can crash into another one??
-#"Carts all move at the same speed; they take turns moving a single step at a time."
+    
+    if debug_output:
+        print_tracks(matrix, trains)
+    
+    print("Result: " + str(final_coord[0]) + "," + str(final_coord[1]))
